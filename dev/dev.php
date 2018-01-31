510d1caa3c3e88d6c4190c31934c320e7d1f30b7
wessimpls <35521941+wessimpls@users.noreply.github.com>
2018-01-31 10:52:45 +0100

    Add files via upload

---
+++ dev/dev.php
@@ -0,0 +1,76 @@
+→Github:</br>
+is a web-based hosting service for version control using git. It is mostly used for computer </br>code. It offers all of the distributed version control and source code management (SCM) </br>functionality of Git as well as adding its own features. It provides access control and </br>several collaboration features such as bug tracking, feature requests, task management, and </br>wikis for every project</br>
+</br>
+</br>
+→Bitbucket:</br>
+Bitbucket is a web-based version control repository hosting service owned by Atlassian, for </br>source code and development projects that use either Mercurial (since launch) or Git (</br>since October 2011) revision control systems. Bitbucket offers both commercial plans </br>and free accounts. It offers free accounts with an unlimited number of private </br>repositories (which can have up to five users in the case of free accounts) as of September </br>2010. Bitbucket integrates with other Atlassian software like Jira, HipChat, Confluence </br>and Bamboo.
+</br>
+</br>
+Difference (Bitbucket & Github):</br>
+</br>
+</br>
+Languanges:</br>
+</br>
+→HTML:</br>
+Hypertext Markup Language (HTML) is the standard markup language for creating web pages and </br>web applications. With Cascading Style Sheets (CSS) and JavaScript it forms a triad of </br>cornerstone technologies for the World Wide Web. Web browsers receive HTML documents </br>from a web server or from local storage and render them into multimedia web pages.</br> HTML describes the structure of a web page semantically and originally included cues for the</br> appearance of the document.
+</br>
+</br>
+→css:</br>
+Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation </br>of a document written in a markup language. Although most often used to set the visual </br>style of web pages and user interfaces written in HTML and XHTML, the language can be</br> applied to any XML document, including plain XML, SVG and XUL, and is applicable to </br>rendering in speech, or on other media. Along with HTML and JavaScript, CSS is a cornerstone </br>technology used by most websites to create visually engaging webpages, user interfaces for </br>web applications, and user interfaces for many mobile applications.</br>
+
+CSS is designed primarily to enable the separation of presentation and content, including </br>aspects such as the layout, colors, and fonts. This separation can improve content</br> accessibility, provide more flexibility and control in the specification of presentation </br>characteristics, enable multiple HTML pages to share formatting by specifying the relevant </br>CSS in a separate .css file, and reduce complexity and repetition in the structural content.</br>
+
+Separation of formatting and content makes it possible to present the same markup page in </br>different styles for different rendering methods, such as on-screen, in print, by voice (via </br>speech-based browser or screen reader), and on Braille-based tactile devices. It can also </br>display the web page differently depending on the screen size or viewing device. Readers can </br>also specify a different style sheet, such as a CSS file stored on their own computer, to </br>override the one the author specified.
+
+Changes to the graphic design of a document (or hundreds of documents) can be applied </br>quickly and easily, by editing a few lines in the CSS file they use, rather than by changing</br> markup in the documents.
+
+The CSS specification describes a priority scheme to determine which style rules apply if </br>more than one rule matches against a particular element. In this so-called cascade, </br>priorities (or weights) are calculated and assigned to rules, so that the results are </br>predictable.</br>
+
+The CSS specifications are maintained by the World Wide Web Consortium (W3C). Internet media</br> type (MIME type) text/css is registered for use with CSS by RFC 2318 (March 1998).</br> The W3C operates a free CSS validation service for CSS documents.</br>
+</br>
+</br>
+→js:</br>
+is a high-level, dynamic, weakly typed, prototype-based, multi-paradigm, and interpreted </br>programming language.</br> Alongside HTML and CSS, JavaScript is one of the three core technologies of World Wide Web content production. </br>It is used to make webpages interactive and provide online programs, including video games.</br> The majority of websites employ it, and all modern web browsers support it without the need for plug-ins by means of a built-in JavaScript engine.</br> Each of the many JavaScript engines represent a different implementation of JavaScript, all based on the ECMAScript specification, with some engines not supporting the spec fully, and with many engines supporting additional features beyond ECMA.
+</br>
+</br>
+→PHP:</br>
+ is a server-side scripting language designed for web development but also used as a </br>general-purpose programming language. Originally created by Rasmus Lerdorf in 1994, the</br> PHP reference implementation is now produced by The PHP Group. PHP originally stood </br>for Personal Home Page, but it now stands for the recursive backronym PHP: </br>Hypertext Preprocessor
+</br>
+</br>
+→Python:</br>
+ is an interpreted high-level programming language for general-purpose programming.</br> Created by Guido van Rossum and first released in 1991, Python has a design philosophy that</br> emphasizes code readability, and a syntax that allows programmers to express concepts</br> in fewer lines of code, notably using significant whitespace.</br> It provides constructs that enable clear programming on both small and large scales.</br>
+
+Python features a dynamic type system and automatic memory management.</br> It supports multiple programming paradigms, including object-oriented, imperative,</br> functional and procedural, and has a large and comprehensive standard library.</br>
+
+Python interpreters are available for many operating systems.</br> CPython, the reference implementation of Python, is open source software and has a community-based development </br>model, as do nearly all of its variant implementations. </br>CPython is managed by the non-profit Python Software Foundation.
+</br>
+</br>
+→Ruby:</br>
+Ruby is a dynamic, reflective, object-oriented, general-purpose programming language.</br> It was designed and developed in the mid-1990s by Yukihiro "Matz" Matsumoto in Japan.
+</br>
+</br>
+Framework:</br>
+</br>
+→Symfony:</br>
+Symfony is a PHP web application framework and a set of reusable PHP components/libraries.</br> Symfony was published as free software on October 18, 2005 and released under the MIT license.
+</br>
+</br>
+→Django:</br>
+Django's primary goal is to ease the creation of complex, database-driven websites.</br> Django emphasizes reusability and "pluggability" of components, rapid development,</br> and the principle of don't repeat yourself. Python is used throughout,</br> even for settings files and data models. Django also provides an optional administrative create, read, update</br></br> and delete interface that is generated dynamically through introspection and configured</br> via admin models.
+</br>
+</br>
+→Ruby on Rails:</br>
+or Rails, is a server-side web application framework written in Ruby under the MIT License.</br> Rails is a model–view–controller (MVC) framework, providing default structures for a </br>database, a web service, and web pages.</br> It encourages and facilitates the use of web standards such as JSON or XML for data transfer, and HTML, CSS and JavaScript for display </br>and user interfacing. </br>In addition to MVC, Rails emphasizes the use of other well-known software</br> engineering patterns and paradigms,</br> including convention over configuration (CoC), don't repeat yourself (DRY), and the active record pattern.
+</br>
+</br>
+
+
+
+
+
+
+
+
+
+
+
